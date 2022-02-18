341bcb1ba7e8cccf08e41719c9c71911c4d7ba87
voidrobyn <92083124+voidrobyn@users.noreply.github.com>
2022-02-18 16:19:13 +0000

    2.9

--- marioGEC/marioGEC/Texture2D.cpp
+++ marioGEC/marioGEC/Texture2D.cpp
@@ -1,1 +1,22 @@
 #include "Texture2D.h"
+#include <SDL_image.h>
+#include <iostream>
+using namespace std;
+
+Texture2D::Texture2D(SDL_Renderer* renderer) {
+
+}
+
+Texture2D::~Texture2D() {
+
+}
+
+bool Texture2D::LoadFromFile(std::string path) {
+
+}
+void Texture2D::Free() {
+
+}
+void Texture2D::Render(Vector2D new_position, SDL_RendererFlip flip, double angle) {
+
+}
